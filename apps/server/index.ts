87cd8bf77387f96dfa28b9d34a6745b109d64a70
Gareth Ng <garethcng@gmail.com>
2024-03-16 11:07:54 -0400

    Scaffolded dashboard

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,24 +1,32 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { prisma } from "./prisma/prisma";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
 const port = process.env.PORT || 3001;
 
 app.get("/", async (req: Request, res: Response) => {
-  // await prisma.ticket.create({
-  //   data: {
-  //     description:
-  //       "Lorem ipsum dolor sit amet consectetur. Nunc nascetur morbi vulputate blandit purus faucibus semper malesuada. Vitae pretium eu nunc dictum vestibulum morbi dolor tempus. Viverra enim dignissim nunc turpis. Amet duis a pellentesque fames fringilla et urna.",
-  //     email: "harris@gmail.com",
-  //     name: "John Harris",
-  //     status: "new",
-  //     subject: "App will not connect",
-  //   },
-  // });
-  res.send("Express + TypeScript Server");
+  const data = await prisma.ticket.findMany();
+  res.send(data);
+});
+
+app.get("/generate", async (req: Request, res: Response) => {
+  await prisma.ticket.create({
+    data: {
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Nunc nascetur morbi vulputate blandit purus faucibus semper malesuada. Vitae pretium eu nunc dictum vestibulum morbi dolor tempus. Viverra enim dignissim nunc turpis. Amet duis a pellentesque fames fringilla et urna.",
+      email: "harris@gmail.com",
+      name: "John Harris",
+      status: "new",
+      subject: "App will not connect",
+    },
+  });
+  // const data = await prisma.ticket.findMany();
+  // res.send(data);
 });
 
 app.listen(port, () => {
---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Spacer from "@/components/Spacer";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+
+export default function Dashboard() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["tickets"],
+    queryFn: getTickets,
+  });
+
+  return (
+    <div className="grid h-full place-content-center">
+      <div className="bg-white [border:1px_solid_#C5CFD3] rounded-6px">
+        <div className="flex bg-[#E9EFF0] py-14px px-16px">
+          <p className="w-[50px]">ID</p>
+          <p className="w-[200px]">Subject</p>
+          <p className="w-[200px]">Email</p>
+          <p className="w-[200px]">Name</p>
+          <p className="w-[100px]">Status</p>
+          <p className="w-[250px]">Date</p>
+        </div>
+
+        {!isLoading &&
+          data?.map((item, index) => {
+            return (
+              <Link href={`/ticket/${item.id}`}>
+                <div className="flex px-16px py-12px">
+                  <p className="w-[50px]">{item.id}</p>
+                  <p className="w-[200px]">{item.subject}</p>
+                  <p className="w-[200px]">{item.email}</p>
+                  <p className="w-[200px]">{item.name}</p>
+                  <p className="w-[100px]">{item.status}</p>
+                  <p className="w-[250px]">{item.createdAt}</p>
+                </div>
+                {index < data.length - 1 ? (
+                  <div className="[border-bottom:1px_solid_#EAEAEA]" />
+                ) : null}
+              </Link>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+const getTickets = async (): Promise<Ticket[]> => {
+  const res = await fetch("http://localhost:3001");
+  const data = await res.json();
+
+  return data;
+};
+
+type Ticket = {
+  id: number;
+  subject: string;
+  name: string;
+  email: string;
+  status: string;
+  createdAt: string;
+};
